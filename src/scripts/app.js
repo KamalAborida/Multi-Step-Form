37d9b6387a6d5da11ef3314b290c4db6fe40d9d4
KamalAborida <kamal.o.aborida@gmail.com>
2023-06-12 21:05:11 +0300

    before error handling

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,130 +1,227 @@
-import * as plansMod from "./plans.js"
-import * as addOnsMod from "./addOns.js"
+import * as stepsMod from "./steps.js"
+
+localStorage.clear()
 
 const nextBtn = document.getElementById("nextBtn")
 const steps = [...document.querySelectorAll(".step")]
 const titleDiv = document.getElementById("titleDiv")
 const contentDiv = document.getElementById("mainDiv")
-let monthlyPayment = true
-let stepNumber = 0
-const stepContent = new Map()
+let totalPrice = 0
+const pricesPlan = [[9, 90], [12, 120], [15, 150]]
+const pricesAddOns = [[1, 10], [1, 10], [2, 20]]
+const stepContentObj = new stepsMod.Steps()
 
-stepContent.set("0", [
-  `
-  <h1>Personal info</h1>
-  <p>Please provide your name, email, address and phone number</p>
-  `,
-  `
-  <div class="infoDivs">
-    <label for="name">Name</label>
-    <input type="text" id="name" placeholder="e.g. Stephen King">
-  </div>
-  <div class="infoDivs">
-    <label for="email">Email</label>
-    <input type="email" id="email" placeholder="e.g. stephenking@lorem.com">
-  </div>
-  <div class="infoDivs">
-    <label for="phoneNumber">Phone Number</label>
-    <input type="number" id="phoneNumber" placeholder="e.g. +1 234 567 890">
-  </div>
-  `,
-])
+function getNextStep() {
+  if (steps[3].classList.contains("active")) {
+    changeContent(4)
+    return
+  }
 
-stepContent.set("1", [
-  `
-  <h1>Select your plan</h1>
-  <p>You have the option of monthly or yearly billing</p>
-  `,
-  `
-  <div class="plan active-plan">
-    <div id="arcade">
-      <img src="./assets/images/icon-arcade.svg" alt="" srcset="">
-      <p class="plan-name">Arcade</p>
-      <p class="plan-price">$9/Monthly</p>
-    </div>
-  </div>
-  <div class="plan">
-    <div id="advanced">
-      <img src="./assets/images/icon-advanced.svg" alt="" srcset="">
-      <p class="plan-name">Advanced</p>
-      <p class="plan-price">$12/Monthly</p>
-    </div>
-  </div>
-  <div class="plan">
-    <div id="pro">
-      <img src="./assets/images/icon-pro.svg" alt="" srcset="">
-      <p class="plan-name">Pro</p>
-      <p class="plan-price">$15/Monthly</p>
-    </div>
-  </div>
-  <div class="month-year-div">
-    <p id="monthly" class="active-paying-time">Monthly</p>
-    <div id="mnth-year-togg" class="toggler-mnth"></div>
-    <p id="yearly">Yearly</p>
-  </div>
-  `,
-])
-
-stepContent.set("2", [
-  `
-  <h1>Pick add-ons</h1>
-  <p>Add-ons help enhance your gaming experience.</p>
-  `,
-  `
-  <div class="addOn">
-    <div class="square"></div>
-    <div class="addOn__info">
-      <p class="addOn__info__title">Online service</p>
-      <p class="addOn__info__details">Access to multiplayer games</p>
-    </div>
-    <p class="addOn__price">+1$/mo</p>
-  </div>
-  <div class="addOn">
-    <div class="square"></div>
-    <div class="addOn__info">
-      <p class="addOn__info__title">Larger storage</p>
-      <p class="addOn__info__details">Extra 1TB of cloud save</p>
-    </div>
-    <p class="addOn__price">+2$/mo</p>
-  </div>
-  <div class="addOn">
-    <div class="square"></div>
-    <div class="addOn__info">
-      <p class="addOn__info__title">Customizable profile</p>
-      <p class="addOn__info__details">Custom theme on your profile</p>
-    </div>
-    <p class="addOn__price">+2$/mo</p>
-  </div>
-  `,
-])
-
-stepContent.set("3", [
-  `
-  <h1>Finishing up</h1>
-  <p>Double-check everything looks OK before confirming.</p>
-  `,
-  `
-  <div>
-    <label for="name">Name</label>
-    <input type="text" id="name">
-  </div>
-  <div>
-    <label for="email">Email</label>
-    <input type="email" id="email">
-  </div>
-  <div>
-    <label for="phoneNumber">Phone Number</label>
-    <input type="number" id="phoneNumber">
-  </div>
-  `,
-])
-
-function changeContent() {
-  
+  for (let i = 0; i <= steps.length; i++) {
+    if (steps[i].classList.contains("active")) {
+      gatherData(i)
+      changeStepCircle(i)
+      changeContent(i + 1)
+      activateCta(i + 1)
+      break
+    } else {
+      continue
+    }
+  }
 }
 
-function stepsHandler() {
-  
+function activateCta(activeIndx) {
+  if (activeIndx == 0) {
+    return
+  } else if (activeIndx == 1) {
+    document
+      .querySelector("#mnth-year-togg")
+      .addEventListener("click", changePaymentState)
+    document.querySelectorAll(".plan").forEach((plan) => {
+      plan.addEventListener("click", changeActivePlan)
+    })
+    return
+  } else if (activeIndx == 2) {
+    document.querySelectorAll(".addOn").forEach((addOn) => {
+      addOn.addEventListener("click", activateAddOn)
+    })
+    if (localStorage.getItem("paymentTime") == "yearly") {
+      let time = document.querySelectorAll(".paymentTime")
+      time.forEach((element, indx) => {
+        element.textContent = "Yearly"
+        element.closest(".addOn").attributes.localStorageItemPrice.value = `${pricesAddOns[indx][1]}`
+        element.closest(".addOn").querySelector(".addOn__price").querySelector("span").textContent = `${pricesAddOns[indx][1]}`
+      })
+    }
+    else {
+      return
+    }
+    return
+  } else if (activeIndx == 3) {
+    showRecipt()
+    if (localStorage.getItem("paymentTime") == "yearly") {
+      let time = document.querySelectorAll(".paymentTime")
+      time.forEach((element) => {
+        element.textContent = "Yearly"
+      })
+    }
+    else {
+      return
+    }
+    return
+  }
 }
 
-nextBtn.addEventListener("click", stepsHandler)
+function gatherData(activeIndx) {
+  if (activeIndx == 0) {
+    return
+  } else if (activeIndx == 1) {
+    let price = document
+      .querySelector(".active-plan")
+      .querySelector(".plan-price")
+      .textContent.split("")
+      .filter((item) => {
+        const regex = /^[0-9]$/
+        return regex.test(item)
+      })
+      .join("")
+    totalPrice += parseInt(price)
+    localStorage.setItem("active-plan", [
+      document.querySelector(".active-plan").querySelector("div").id,
+      price,
+    ])
+    if (
+      document.querySelector("#mnth-year-togg").classList[0] == "toggler-mnth"
+    ) {
+      localStorage.setItem("paymentTime", "monthly")
+    } else {
+      localStorage.setItem("paymentTime", "yearly")
+    }
+    return
+  } else if (activeIndx == 2) {
+    document.querySelectorAll(".active-addOn").forEach((addOn, indx) => {
+      localStorage.setItem(
+        `addOn${indx}`,
+        addOn.attributes.getNamedItem("localstorageitem").value
+      )
+      localStorage.setItem(
+        `addOnPrice${indx}`,
+        addOn.attributes.getNamedItem("localstorageitemPrice").value
+      )
+      // console.log(addOn.attributes.getNamedItem("localstorageitemPrice").value);
+      totalPrice += parseInt(
+        addOn.attributes.getNamedItem("localstorageitemPrice").value
+      )
+    })
+    return
+  } else if (activeIndx == 3) {
+    return
+  }
+}
+
+function showRecipt() {
+  document.querySelector(".recipt__plan__currentPlan").textContent = `${
+    makeRecipt()[1]
+  } (${makeRecipt()[2]})`
+  if (makeRecipt()[2] == "monthly") {
+    document.querySelector(".recipt__plan__price").textContent = `$${
+      makeRecipt()[0]
+    }/mo`
+  } else {
+    document.querySelector(".recipt__plan__price").textContent = `$${
+      makeRecipt()[0]
+    }/year`
+  }
+  document.querySelector(".recipt__addOns-div").innerHTML = ``
+  let addOn = document.createElement("div")
+  addOn.classList.add("recipt__addOns")
+  for (let i = 0; i < 3; i++) {
+    if (localStorage.getItem(`addOn${i}`)) {
+      let clonedAddOn = addOn.cloneNode(true)
+      clonedAddOn.innerHTML = `
+      <p class="recipt__label">${localStorage.getItem(`addOn${i}`)}</p>
+      <p class="recipt__addOns__price">${localStorage.getItem(
+        `addOnPrice${i}`
+      )}/<span class="paymentTime">Monthly</span></p>
+      `
+      document.querySelector(".recipt__addOns-div").append(clonedAddOn)
+    }
+  }
+  document.querySelector(
+    ".recipt__total__price"
+  ).textContent = `$${totalPrice}/${
+    localStorage.getItem("paymentTime") == "monthly" ? "mo" : "year"
+  }`
+}
+
+function makeRecipt() {
+  let price = localStorage
+    .getItem("active-plan")
+    .split("")
+    .filter((item) => {
+      const regex = /^[0-9]$/
+      return regex.test(item)
+    })
+    .join("")
+  let plan = localStorage
+    .getItem("active-plan")
+    .split("")
+    .filter((item) => {
+      const regex = /^[a-z]$/
+      return regex.test(item)
+    })
+    .join("")
+  let paymentTime = localStorage.getItem("paymentTime")
+  return [price, plan, paymentTime]
+}
+
+function changeStepCircle(circleIndx) {
+  steps[circleIndx + 1].classList.add("active")
+  steps[circleIndx].classList.remove("active")
+}
+
+function changeContent(indxOfContent) {
+  titleDiv.innerHTML = stepContentObj.stepContent.get(`${indxOfContent}`)[0]
+  contentDiv.innerHTML = stepContentObj.stepContent.get(`${indxOfContent}`)[1]
+}
+
+function changePaymentState(e) {
+  e.target.classList.toggle("toggler-mnth")
+  e.target.classList.toggle("toggler-year")
+  let time = document.querySelectorAll(".paymentTime")
+  time.forEach((element, indx) => {
+    if (e.target.classList.contains("toggler-mnth")) {
+      element.textContent = "Monthly"
+      document.getElementById("monthly").classList.add("active-paying-time")
+      document.getElementById("yearly").classList.remove("active-paying-time")
+      element.closest(".plan").querySelector("div").attributes.planPrice.value = `${pricesPlan[indx][0]}`
+      element.closest(".plan").querySelector(".plan-price").querySelector("span").textContent = `${pricesPlan[indx][0]}`
+    } else {
+      element.textContent = "Yearly"
+      document.getElementById("monthly").classList.remove("active-paying-time")
+      document.getElementById("yearly").classList.add("active-paying-time")
+      element.closest(".plan").querySelector("div").attributes.planPrice.value = `${pricesPlan[indx][1]}`
+      element.closest(".plan").querySelector(".plan-price").querySelector("span").textContent = `${pricesPlan[indx][1]}`
+    }
+  })
+}
+
+function changeActivePlan(e) {
+  document.querySelectorAll(".plan").forEach((plan) => {
+    if (plan.classList.contains("active-plan")) {
+      plan.classList.remove("active-plan")
+    }
+  })
+  e.target.closest(".plan").classList.add("active-plan")
+}
+
+function activateAddOn(e) {
+  e.target.closest(".addOn").classList.toggle("active-addOn")
+  e.target
+    .closest(".addOn")
+    .querySelector(".square")
+    .classList.toggle("active-square")
+}
+
+changeContent(0)
+nextBtn.addEventListener("click", getNextStep)
---
+++ src/scripts/steps.js
@@ -0,0 +1,143 @@
+export class Steps {
+  stepContent
+
+  constructor() {
+    this.stepContent = new Map()
+
+    this.stepContent.set("0", [
+      `
+      <h1>Personal info</h1>
+      <p>Please provide your name, email, address and phone number</p>
+      `,
+      `
+      <div class="infoDivs">
+        <label for="name">Name</label>
+        <input type="text" id="name" placeholder="e.g. Stephen King">
+      </div>
+      <div class="infoDivs">
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="e.g. stephenking@lorem.com">
+      </div>
+      <div class="infoDivs">
+        <label for="phoneNumber">Phone Number</label>
+        <input type="number" id="phoneNumber" placeholder="e.g. +1 234 567 890">
+      </div>
+      `,
+    ])
+    
+    this.stepContent.set("1", [
+      `
+      <h1>Select your plan</h1>
+      <p>You have the option of monthly or yearly billing</p>
+      `,
+      `
+      <div class="plans-div">
+        <div class="plan active-plan">
+          <div id="arcade" planPrice="9">
+            <img src="./assets/images/icon-arcade.svg" alt="" srcset="">
+            <p class="plan-name">Arcade</p>
+            <p class="plan-price">$<span>9</span>/<span class="paymentTime">Monthly</span></p>
+          </div>
+        </div>
+        <div class="plan">
+          <div id="advanced" planPrice="9">
+            <img src="./assets/images/icon-advanced.svg" alt="" srcset="">
+            <p class="plan-name">Advanced</p>
+            <p class="plan-price">$<span>12</span>/<span class="paymentTime">Monthly</span></p>
+          </div>
+        </div>
+        <div class="plan">
+          <div id="pro" planPrice="15">
+            <img src="./assets/images/icon-pro.svg" alt="" srcset="">
+            <p class="plan-name">Pro</p>
+            <p class="plan-price">$<span>15</span>/<span class="paymentTime">Monthly</span></p>
+          </div>
+        </div>
+      </div>
+      <div class="month-year-div">
+        <p id="monthly" class="active-paying-time">Monthly</p>
+        <div id="mnth-year-togg" class="toggler-mnth"></div>
+        <p id="yearly">Yearly</p>
+      </div>
+      `,
+    ])
+    
+    this.stepContent.set("2", [
+      `
+      <h1>Pick add-ons</h1>
+      <p>Add-ons help enhance your gaming experience.</p>
+      `,
+      `
+      <div class="addOn" localStorageItem="Online service" localStorageItemPrice="1">
+        <div class="square"></div>
+        <div class="addOn__info">
+          <p class="addOn__info__title">Online service</p>
+          <p class="addOn__info__details">Access to multiplayer games</p>
+        </div>
+        <p class="addOn__price">+<span>1</span>$/<span class="paymentTime">Monthly</span></p>
+      </div>
+      <div class="addOn" localStorageItem="Larger storage" localStorageItemPrice="2">
+        <div class="square"></div>
+        <div class="addOn__info">
+          <p class="addOn__info__title">Larger storage</p>
+          <p class="addOn__info__details">Extra 1TB of cloud save</p>
+        </div>
+        <p class="addOn__price">+<span>1</span>$/<span class="paymentTime">Monthly</span></p>
+      </div>
+      <div class="addOn" localStorageItem="Customizable profile" localStorageItemPrice="1">
+        <div class="square"></div>
+        <div class="addOn__info">
+          <p class="addOn__info__title">Customizable profile</p>
+          <p class="addOn__info__details">Custom theme on your profile</p>
+        </div>
+        <p class="addOn__price">+<span>2</span>$/<span class="paymentTime">Monthly</span></p>
+      </div>
+      `,
+    ])
+    
+    this.stepContent.set("3", [
+      `
+      <h1>Finishing up</h1>
+      <p>Double-check everything looks OK before confirming.</p>
+      `,
+      `
+      <div class="recipt">
+        <div class="recipt__plan">
+          <p class="recipt__plan__currentPlan">arcade (<span class="paymentTime">Monthly</span>)</p>
+          <a class="recipt__label link">change</a>
+          <p class="recipt__plan__price">$9/<span class="paymentTime">Monthly</span></p>
+        </div>
+        <hr>
+        <div class="recipt__addOns-div">
+          <div class="recipt__addOns">
+            <p class="recipt__label">Online service</p>
+            <p class="recipt__addOns__price">$1/<span class="paymentTime">Monthly</span></p>
+          </div>
+          <div class="recipt__addOns">
+            <p class="recipt__label">Large storage</p>
+            <p class="recipt__addOns__price">$2/<span class="paymentTime">Monthly</span></p>
+          </div>
+        </div>
+      </div>
+      <div class="recipt__total">
+        <p class="recipt__label">Total (<span class="paymentTime">Monthly</span>)</p>
+        <p class="recipt__total__price">$12/<span class="paymentTime">Monthly</span></p>
+      </div>
+      `,
+    ])
+    
+    this.stepContent.set("4", [
+      `
+      <img>
+      <h1>Thank you</h1>
+      `,
+      `
+      <div>
+        Thank you! Thanks for confirming your subscription! We hope you have fun
+        using our platform. If you ever need support, please feel free to email us
+        at support@loremgaming.com.
+      </div>
+      `,
+    ])
+  }
+}
